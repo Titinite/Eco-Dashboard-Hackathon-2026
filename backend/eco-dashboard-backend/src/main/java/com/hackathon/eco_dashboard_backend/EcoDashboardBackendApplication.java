9fd52522859577137d9fba3a53021f4f540850c6
Titinite <thibault.leray@supdevinci-edu.fr>
2026-03-16 11:41:29 +0100

    Initial commit

---
+++ backend/eco-dashboard-backend/src/main/java/com/hackathon/eco_dashboard_backend/EcoDashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.eco_dashboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoDashboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoDashboardBackendApplication.class, args);
+	}
+
+}
---
+++ backend/eco-dashboard-backend/src/test/java/com/hackathon/eco_dashboard_backend/EcoDashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.eco_dashboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoDashboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
